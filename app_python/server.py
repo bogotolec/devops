c42b929da90c385d2ad5362b69506712a7e3f166
bogotolec <i.makarenko@innopolis.university>
2021-09-28 21:28:27 +0300

    Lab12 complete

--- app_python/server.py
+++ app_python/server.py
@@ -7,8 +7,23 @@ class TimeReciever:
     def get_time():
         return datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
 
+def get_visits():
+    with open("logs/visits.txt", "r") as o:
+        return o.read()
+
+def inc_visits():
+    current = int(get_visits())
+    current += 1
+    with open("logs/visits.txt", "w") as w:
+        w.write(str(current))
+
+def create_visits():
+    with open("logs/visits.txt", "w") as w:
+        w.write("0")
+
 class MyHttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+
 
         # Send status code, since we don't operate on
         # any data, we always send 200 (OK).
@@ -17,12 +32,19 @@ class MyHttpHandler(BaseHTTPRequestHandler):
         # Send usual headers for text web pages.
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        
-        time = TimeReciever.get_time()
 
-        self.wfile.write(time.encode('utf-8'))
+        content = ""
+
+        if self.path == "/visits":
+            content = get_visits().encode('utf-8')
+        else:
+            inc_visits()
+            content = TimeReciever.get_time().encode('utf-8')
+
+        self.wfile.write(content)
 
 
 if __name__ == '__main__':
+    create_visits()
     server = HTTPServer(ADDR, MyHttpHandler)
     server.serve_forever()
